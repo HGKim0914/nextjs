34dc0c88cf976e57eadf7f442c514be548a7588c
HGKIM0914 <icandoit0914@gmail.com>
2023-03-23 20:38:43 -0700

    rewrites and redirects functionality

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
-import Navbar from "../components/navbar";
+import Layout from "@/components/layout";
+import "../styles/globals.css";
 
 export default function App({Component, pageProps}) {
     return (
-        <div>
-            <Navbar />
+        <Layout>
             <Component {...pageProps} />
-        </div>
+        </Layout>
     )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,11 @@
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
+import Seo from "@/components/Seo";
+import Head from "next/head";
 
 export default function About() {
     return (
         <div>
+            <Seo title="About" />
             <h1>about page</h1>
         </div>
     )
